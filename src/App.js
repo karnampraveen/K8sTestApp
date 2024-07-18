eadba38fe7cc95c17b82440d6f2dcc6b0a0adf52
karnampraveen <168419934+karnampraveen@users.noreply.github.com>
2024-07-18 15:24:10 +0100

    Update App.js as 2nd version

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>My 1st React App</h1>
+        <h1>My 2nd React App</h1>
         <p>Everyday is a school day</p>
       </header>
       <div className="image-wrapper">
